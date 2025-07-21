709e4ba7deec5bfd9eddb9e9e2c927f753e4c29a
ArthurGuti <arthurzinhosfs@gmail.com>
2025-07-21 14:00:40 -0300

    Rede social adicionada, estilização de redes prontas- header com scroll feito!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function enviarWhats(event) {
-   event.preventDefault(); // Evita que a tela recarregue ao enviar o formulário
+  event.preventDefault(); // Evita que a tela recarregue ao enviar o formulário
 
   const nome = document.getElementById("nome").value;
   const mensagem = document.getElementById("mensagem").value;
@@ -10,23 +10,31 @@ function enviarWhats(event) {
 
   const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
 
-  const menuLinks = document.querySelectorAll('.menu-link');
-  const nav = document.querySelector('.navegacao');
+  const menuLinks = document.querySelectorAll(".menu-link");
+  const nav = document.querySelector(".navegacao");
 
   menuLinks.forEach((link, index) => {
-    link.addEventListener('mouseenter', () => {
-      nav.style.setProperty('--index', index + 1);
-      nav.style.setProperty('--items', menuLinks.length);
-      nav.style.setProperty('--width', `calc(100% / ${menuLinks.length})`);
-      nav.style.setProperty('--left', `calc(100% / ${menuLinks.length} * ${index})`);
-      nav.style.setProperty('--opacity', '1');
-      nav.classList.add('active-line');
+    link.addEventListener("mouseenter", () => {
+      nav.style.setProperty("--index", index + 1);
+      nav.style.setProperty("--items", menuLinks.length);
+      nav.style.setProperty("--width", `calc(100% / ${menuLinks.length})`);
+      nav.style.setProperty(
+        "--left",
+        `calc(100% / ${menuLinks.length} * ${index})`
+      );
+      nav.style.setProperty("--opacity", "1");
+      nav.classList.add("active-line");
     });
 
-    link.addEventListener('mouseleave', () => {
-      nav.classList.remove('active-line');
+    link.addEventListener("mouseleave", () => {
+      nav.classList.remove("active-line");
     });
   });
 
   window.open(url, "_blank");
 }
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("#header");
+  header.classList.toggle("rolagem", window.scrollY > 0);
+});
